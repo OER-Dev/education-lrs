d73c05cdae6ae0376c36acb6ef0939d26e86e3ab
Christer <me@christer.io>
2022-02-21 22:48:35 +0100

    Addes cors

--- pages/api/addresults/index.js
+++ pages/api/addresults/index.js
@@ -1,4 +1,15 @@
 import { MongoClient } from 'mongodb'
+import Cors from 'cors'
+import initMiddleware from '../../../lib/init-middleware'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
 const uri = process.env.MONGODB_URI
 
