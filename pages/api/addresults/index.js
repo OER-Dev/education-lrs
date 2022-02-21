24087a5fb1e0688871d63221902a3cb6504db797
Christer <me@christer.io>
2022-02-21 15:04:42 +0100

    Added timestamp on result posts

--- pages/api/addresults/index.js
+++ pages/api/addresults/index.js
@@ -12,17 +12,24 @@ const options = {
 export default function handler(req, res) {
     async function addResults() {
         const client = new MongoClient(uri, options);
+        const statement = JSON.stringify(req.body);
+        const time = new Date();
+        const response = {
+            "appId": 1,
+            "timestamp": time,
+            "results": statement
+        }
 
         try {
             await client.connect();
 
-            await client.db("assessment").collection("results").insertOne(req.body);
+            await client.db("assessment").collection("results").insertOne(response);
 
         } catch (e) {
             console.error(e);
             res.status(400).json("Error")
         } finally {
-            res.status(200).json(req.body)
+            res.status(200).json(response)
             await client.close();
         }
 
