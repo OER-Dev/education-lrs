847534188d11a49a79e776c032da97b0b49b10ae
Christer <me@christer.io>
2022-03-15 12:51:06 +0100

    added level to endpoint

--- data/h5ptopic.js
+++ data/h5ptopic.js
@@ -1,119 +1,147 @@
 export const h5ptopic = [
-        {
-          "id": "8990",
-          "topic": "counting"
-        },
-        {
-          "id": "8991",
-          "topic": "counting"
-        },
-        {
-          "id": "8992",
-          "topic": "counting"
-        },
-        {
-          "id": "8993",
-          "topic": "counting"
-        },
-        {
-          "id": "8994",
-          "topic": "counting"
-        },
-        {
-          "id": "8995",
-          "topic": "counting"
-        },
-        {
-          "id": "8996",
-          "topic": "counting"
-        },
-        {
-          "id": "8997",
-          "topic": "counting"
-        },
-        {
-          "id": "8998",
-          "topic": "counting"
-        },
-        {
-          "id": "8999",
-          "topic": "counting"
-        },
-        {
-          "id": "9000",
-          "topic": "counting"
-        },
-        {
-          "id": "9001",
-          "topic": "counting-objects"
-        },
-        {
-          "id": "9002",
-          "topic": "counting-objects"
-        },
-        {
-          "id": "9003",
-          "topic": "counting-objects"
-        },
-        {
-          "id": "9004",
-          "topic": "counting-objects"
-        },
-        {
-          "id": "9005",
-          "topic": "counting-objects"
-        },
-        {
-          "id": "9006",
-          "topic": "counting-objects"
-        },
-        {
-          "id": "9007",
-          "topic": "counting-objects"
-        },
-        {
-          "id": "9008",
-          "topic": "counting-objects"
-        },
-        {
-          "id": "9009",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9010",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9011",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9012",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9013",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9014",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9015",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9016",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9017",
-          "topic": "comparing-small-numbers"
-        },
-        {
-          "id": "9018",
-          "topic": "comparing-small-numbers"
-        }
-       
+    {
+      "id": "8990",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8991",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8992",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8993",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8994",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8995",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8996",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8997",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8998",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "8999",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "9000",
+      "topic": "counting",
+      "level": 1
+    },
+    {
+      "id": "9001",
+      "topic": "counting-objects",
+      "level": 2
+    },
+    {
+      "id": "9002",
+      "topic": "counting-objects",
+      "level": 2
+    },
+    {
+      "id": "9003",
+      "topic": "counting-objects",
+      "level": 2
+    },
+    {
+      "id": "9004",
+      "topic": "counting-objects",
+      "level": 2
+    },
+    {
+      "id": "9005",
+      "topic": "counting-objects",
+      "level": 2
+    },
+    {
+      "id": "9006",
+      "topic": "counting-objects",
+      "level": 2
+    },
+    {
+      "id": "9007",
+      "topic": "counting-objects",
+      "level": 2
+    },
+    {
+      "id": "9008",
+      "topic": "counting-objects",
+      "level": 2
+    },
+    {
+      "id": "9009",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9010",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9011",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9012",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9013",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9014",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9015",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9016",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9017",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    },
+    {
+      "id": "9018",
+      "topic": "comparing-small-numbers",
+      "level": 3
+    }
    ]
